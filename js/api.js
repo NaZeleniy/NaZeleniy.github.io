3b41c240215de5ad184cb5cf23c22c4bbe60cc93
NaZeleniy <nazeleny@tutamail.com>
2026-03-28 06:36:19 +0700

    switch to https

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
 // Base URL for the backend API.
 // Empty string = same server (for local dev via Go).
 // TODO: before deploying to GitHub Pages, set this to your production backend URL.
-const API_BASE = 'http://87.121.79.221:8080'
+const API_BASE = 'https://nazeleniy.mooo.com'
