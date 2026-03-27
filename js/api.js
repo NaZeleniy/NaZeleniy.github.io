c2025ce4cea4f21c4950a7df71b1d2628b15ef37
NaZeleniy <nazeleny@tutamail.com>
2026-03-28 06:14:22 +0700

    set api base url

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
 // Base URL for the backend API.
 // Empty string = same server (for local dev via Go).
 // TODO: before deploying to GitHub Pages, set this to your production backend URL.
-const API_BASE = ''
+const API_BASE = 'http://87.121.79.221:8080'
