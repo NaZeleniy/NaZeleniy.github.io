395825acbbb0c7597ffadcd41e4b62cb68035434
NaZeleniy <nazeleny@tutamail.com>
2026-03-31 09:35:15 +0700

    add metrics

--- js/header.js
+++ js/header.js
@@ -1,16 +1,6 @@
 function renderHeader(activePage) {
   _renderSidebar(activePage)
   if (activePage !== 'settings') _renderSearch(activePage)
-  _renderLiveInternet()
-}
-
-function _renderLiveInternet() {
-  new Image().src = 'https://counter.yadro.ru/hit?r'
-    + encodeURIComponent(document.referrer)
-    + (typeof screen === 'undefined' ? '' : ';s' + screen.width + '*' + screen.height + '*' + (screen.colorDepth ? screen.colorDepth : screen.pixelDepth))
-    + ';u' + encodeURIComponent(document.URL)
-    + ';h' + encodeURIComponent(document.title.substring(0, 150))
-    + ';' + Math.random()
 }
 
 function _renderSidebar(activePage) {
