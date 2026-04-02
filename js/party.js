f9b19a9f03271b0f790459abe368ea1517bc3112
NaZeleniy <nazeleny@tutamail.com>
2026-04-02 21:04:41 +0700

    refactor

--- js/party.js
+++ js/party.js
@@ -218,6 +218,8 @@ window.addEventListener('message', e => {
   const data = e.data
   if (!data || data.type !== 'playerEvent') return
 
+  console.log('[playerEvent]', JSON.stringify(data))
+
   const ev = data.event
 
   if (ev === 'ready' || ev === 'sync_ready') {
