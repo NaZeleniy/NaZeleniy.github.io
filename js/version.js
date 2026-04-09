0426b9ce8a5b61c6c27c9db0b1992f4efab6a20e
NaZeleniy <nazeleny@tutamail.com>
2026-04-09 23:21:50 +0700

    add inline svg

--- js/version.js
+++ js/version.js
@@ -1,7 +1,7 @@
 // Если браузер закэшировал старый HTML — форсируем перезагрузку.
 // Версия обновляется автоматически pre-commit хуком вместе с остальными ?v=
 ;(function () {
-  var V = 'v=1775751364'
+  var V = 'v=1775751710'
   var key = 'nz_page_v'
   var stored = localStorage.getItem(key)
   if (stored !== V) {
